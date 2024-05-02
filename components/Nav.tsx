f78a5c29d36bbd1345c5e4dea7346a35bcede1ef
Kanuck-mk2 <141983009+Kanuck-mk2@users.noreply.github.com>
2024-05-01 22:08:27 -0600

    added a index.ts in constants directory

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,22 +1,12 @@
-'use client';
+'use client'
 
+import { navbarLinks } from '../constants';
 import { Link as ScrollLink } from 'react-scroll';
-
-const Links = [
-  { name: 'home', target: 'home', offset: -100 },
-  { name: 'about', target: 'about', offset: -80 },
-  { name: 'class', target: 'class', offset: -80 },
-  { name: 'team', target: 'team', offset: 0 },
-  { name: 'prices', target: 'prices', offset: -40 },
-  { name: 'testimonial', target: 'testimonial', offset: 0 },
-  { name: 'blog', target: 'blog', offset: 0 },
-  { name: 'contact', target: 'contact', offset: 0 },
-];
 
 const Nav = ({ containerStyles }: { containerStyles: string }) => {
   return (
     <nav className={`${containerStyles}`}>
-      {Links.map((link, index) => {
+      {navbarLinks.map((link, index) => {
         return (
           <ScrollLink
             offset={link.offset}
---
+++ constants/index.ts
@@ -0,0 +1,19 @@
+
+
+export interface NavbarLink {
+    name: string;
+    target: string;
+    offset: number;
+  }
+  
+  export const navbarLinks: NavbarLink[] = [
+    { name: 'home', target: 'home', offset: -100 },
+    { name: 'about', target: 'about', offset: -80 },
+    { name: 'class', target: 'class', offset: -80 },
+    { name: 'team', target: 'team', offset: 0 },
+    { name: 'prices', target: 'prices', offset: -40 },
+    { name: 'testimonial', target: 'testimonial', offset: 0 },
+    { name: 'blog', target: 'blog', offset: 0 },
+    { name: 'contact', target: 'contact', offset: 0 },
+  ];
+  
