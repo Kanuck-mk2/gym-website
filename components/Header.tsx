c4219f46eac55aa1f7c1085bb5b6317249db5ce4
Kanuck-mk2 <141983009+Kanuck-mk2@users.noreply.github.com>
2024-05-03 18:08:25 -0600

    added MobileNav and stlying

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,44 @@
+'use client';
+
+import Link from 'next/link';
+import MobileNav from './MobileNav';
 import Nav from './Nav';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+  const [headerActive, setHeaderActive] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHeaderActive(window.scrollY > 50);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="fixed left-0 right-0 w-full bg-white">
-      <div className="container mx-auto">
-        <Nav containerStyles=" py-12 flex gap-4  " />
+    <header
+      className={`${
+        headerActive ? 'h-[100px]' : 'h-[124px]'
+      }fixed max-w-[1920px] top-0 left-0 right-0 w-full bg-primary-200 h-[100px] transition-all z-50`}
+    >
+      <div className="container mx-auto h-full flex items-center justify-between">
+        <Link href="">
+          <Image
+            src={'/assets/img/logo.png'}
+            width={117}
+            height={55}
+            alt="logo"
+          />
+        </Link>
+
+        <MobileNav containerStyles=" text-white xl:hidden" />
+        <Nav containerStyles=" flex gap-4 text-white hidden xl:flex " />
       </div>
     </header>
   );
---
+++ components/MobileNav.tsx
@@ -0,0 +1,9 @@
+
+
+const MobileNav = ({containerStyles} : {containerStyles: string}) => {
+  return (
+    <nav>MobileNav</nav>
+  )
+}
+
+export default MobileNav
