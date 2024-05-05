39303ba323b4b14ac1836b1e78d4bb5bf94f3ba3
Kanuck-mk2 <141983009+Kanuck-mk2@users.noreply.github.com>
2024-05-04 21:29:11 -0600

    styling and positioning

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import HeroSlider from './HeroSlider';
 const Hero = () => {
   return (
     <section
-      className="h-[120vh] lg:h-[180vh] bg-hero bg-cover bg-center bg-no-repeat"
+      className="h-[120vh] lg:h-[160vh] bg-hero bg-cover bg-center bg-no-repeat"
       id="home"
     >
       <div className="container mx-auto h-full ">
--- components/HeroSlider.tsx
+++ components/HeroSlider.tsx
@@ -17,7 +17,7 @@ const HeroSlider = () => {
         <div className="h-full flex justify-start pt-48 ">
           <div className="flex flex-col items-center lg:items-start lg:max-w-[700px] -mt-9">
             <motion.h1
-              className="h1 text-center lg:text-left mb-2 font-anta"
+              className="lg:text-8xl text-gray-200 text-center lg:text-left mb-2 font-anta sm:text-7xl"
               variants={fadeIn('up', 0.4)}
               initial="hidden"
               whileInView="visible"
@@ -37,42 +37,64 @@ const HeroSlider = () => {
               engaged in realizing your goals.
             </motion.p>
             <motion.div
-            variants={fadeIn('up', 0.8)}
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: false, amount: 0.2 }}>
-            <CustomButton
-              text="Get started"
-              containerStyles="w-[196px] h-[62px] bg-accent rounded-full font-anta "
-            />
+              variants={fadeIn('up', 0.8)}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: false, amount: 0.2 }}
+            >
+              <CustomButton
+                text="Get started"
+                containerStyles="w-[196px] h-[62px] bg-accent rounded-full font-anta "
+              />
             </motion.div>
           </div>
         </div>
       </SwiperSlide>
+
       <SwiperSlide>
         <div className="h-full flex justify-start pt-48 ">
           <div className="flex flex-col items-center lg:items-start lg:max-w-[700px] -mt-9">
-            <h1 className="lg:text-8xl text-gray-200 text-center lg:text-left mb-2 font-anta sm:text-5xl">
+            <motion.h1
+              className="lg:text-7xl text-gray-200 text-center lg:text-left mb-2 font-anta sm:text-5xl"
+              variants={fadeIn('up', 0.4)}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: false, amount: 0.2 }}
+            >
               Our <span className="text-accent">Team </span>
               of expert <span className="text-accent">coaches</span> to guide
-              and motivate to your <span className="text-accent">peak</span>{' '}
-              potiental
-            </h1>
-            <p className="text-gray-200 italic font-semibold text-center lg:text-left mb-4">
+              and <span className="text-accent">motivate</span> to your{' '}
+              <span className="text-accent">peak</span> potiental
+            </motion.h1>
+            <motion.p
+              className="text-gray-200 italic font-semibold text-center lg:text-left mb-4"
+              variants={fadeIn('up', 0.6)}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: false, amount: 0.2 }}
+            >
               Our trainers are highly qualified and experienced with a wide
               range of clients. Our team is equipped and knowledgable in meeting
               your goals.
-            </p>
-            <CustomButton
-              text="Learn More"
-              containerStyles="w-[196px] h-[62px] bg-accent rounded-full font-anta "
-            />
+            </motion.p>
+            <motion.div
+              variants={fadeIn('up', 0.8)}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: false, amount: 0.2 }}
+            >
+              <CustomButton
+                text="Learn More"
+                containerStyles="w-[196px] h-[62px] bg-accent rounded-full font-anta "
+              />
+            </motion.div>
           </div>
         </div>
       </SwiperSlide>
       <SwiperNavButtons
-        containerStyles="absolute bottom-2 lg:bottom-0 right-0 h-[130px] w-full lg:w-[700px] z-50 flex sm:justify-end sm:py-9 sm:mr-50  lg:justify-end mr-60 mb-7 gap-2"
-        btnStyles="border border-accent text-white w-[56px] h-[56px] flex justify-center
+        containerStyles="absolute bottom-2 lg:right-0 lg:mb-[200px] lg:mr-[430px] h-[120px] w-full lg:w-[130px] z-50 
+        flex justify-center sm:justify-center sm:py-9 sm:mr-50   border   gap-2"
+        btnStyles="border border-accent rounded-full text-white w-[56px] h-[56px] flex justify-center
                  items-center hover:bg-accent transition-all duration-300"
         iconStyles="text-2xl"
       />
