71f5a895b7780846ca808e62eb8a7c4a9c35e662
Kanuck-mk2 <141983009+Kanuck-mk2@users.noreply.github.com>
2024-05-06 17:22:08 -0600

    updated classes section

--- components/Classes.tsx
+++ components/Classes.tsx
@@ -9,8 +9,54 @@ import { classes } from '@/constants';
 
 const Classes = () => {
   return (
-    <section className="h-screen bg-green-300" id="classes">
-      Classes
+    <section id="class" className="bg-black">
+      <motion.div
+        className="grid grid-cols-1 lg:grid-cols-2 "
+        variants={fadeIn('up', 0.6)}
+        initial="childHidden"
+        whileInView={'childVisible'}
+        viewport={{ once: false, amount: 0.2 }}
+      >
+        {classes.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="relative flex flex-col justify-center items-center"
+            >
+              <div className="relative w-full h-[300px] lg:h-[486px]">
+                <Image src={item.img} layout="fill" objectFit="cover" alt="/" />
+                <div className="absolute inset-0 flex flex-col items-center justify-center gap-4">
+                  <motion.h3
+                    className="h3 text-accent"
+                    variants={fadeIn('up', 0.4)}
+                    initial="childHidden"
+                    whileInView={'childVisible'}
+                    viewport={{ once: false, amount: 0.2 }}
+                  >
+                    {item.name}
+                  </motion.h3>
+                  <motion.p className="text-white"
+                   variants={fadeIn('up', 0.6)}
+                   initial="childHidden"
+                   whileInView={'childVisible'}
+                   viewport={{ once: false, amount: 0.2 }}>{item.description}</motion.p>
+
+                   <motion.div
+                    variants={fadeIn('up', 0.8)}
+                    initial="childHidden"
+                    whileInView={'childVisible'}
+                    viewport={{ once: false, amount: 0.2 }}>
+                  <CustomButton
+                    containerStyles="w-[164px] h-[46px] rounded-full"
+                    text="Read More"
+                  />
+                  </motion.div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </motion.div>
     </section>
   );
 };
--- constants/index.ts
+++ constants/index.ts
@@ -14,7 +14,7 @@ export interface NavbarLink {
 export const navbarLinks: NavbarLink[] = [
   { name: 'home', target: 'home', offset: -100 },
   { name: 'about', target: 'about', offset: -80 },
-  { name: 'classes', target: 'classes', offset: -80 },
+  { name: 'classes', target: 'class', offset: -80 },
   { name: 'team', target: 'team', offset: 0 },
   { name: 'prices', target: 'prices', offset: -40 },
   { name: 'testimonial', target: 'testimonial', offset: 0 },
