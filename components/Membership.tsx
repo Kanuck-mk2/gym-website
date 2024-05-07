67d26350027cabb2f2a6e640c7ac5809a1a31434
Kanuck-mk2 <141983009+Kanuck-mk2@users.noreply.github.com>
2024-05-06 19:13:57 -0600

    update

--- components/Membership.tsx
+++ components/Membership.tsx
@@ -1,5 +1,16 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { fadeIn } from '@/constants/animations';
+
 const Membership = () => {
-  return <section className="h-screen bg-pink-300" id="prices">Membership</section>;
+  return (
+    <section className="h-screen bg-pink-300" id="prices">
+      <div>
+        <h2>Membership Plans</h2>
+      </div>
+    </section>
+  );
 };
 
 export default Membership;
--- components/Team.tsx
+++ components/Team.tsx
@@ -11,25 +11,47 @@ import { trainerData } from '@/constants';
 
 const Team = () => {
   return (
-    <section className="bg-black py-12 lg:h-[110vh] " id="team">
-      <div className="container mx-auto h-full flex flex-col items-center justify-center">
-        <h2 className="text-4xl"> Our Trainers</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 m-6">
+    <section className="bg-black p-16 lg:h-[120vh]" id="team">
+      <div className="container mx-auto h-full flex flex-col items-center justify-evenly">
+        <motion.h2
+          className="text-4xl mb-6"
+          variants={fadeIn('up', 0.4)}
+          initial="childHidden"
+          whileInView={'childVisible'}
+          viewport={{ once: false, amount: 0.2 }}
+        >
+          {' '}
+          Our Trainers
+        </motion.h2>
+        <motion.div
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 mr-9"
+          variants={fadeIn('up', 0.6)}
+          initial="childHidden"
+          whileInView={'childVisible'}
+          viewport={{ once: false, amount: 0.2 }}
+        >
           {trainerData.map((trainer, index) => {
             return (
-              <div key={index}>
+              <div
+                className="flex flex-col items-center text-center"
+                key={index}
+              >
                 <div className="relative w-[320px] h-[360px] mx-auto mb-4">
                   <Image src={trainer.image} fill alt="/" />
                 </div>
-                <h4>{trainer.name}</h4>
-                <p>{trainer.role}</p>
-                <p>{trainer.description}</p>
-                <div>
+                <h4 className="h4 mb-2">{trainer.name}</h4>
+                <p className="uppercase text-xs tracking-[3px] mb-2 text-gray-200">
+                  {trainer.role}
+                </p>
+                <p className="mb-6 text-gray-200 max-w-[320px] mx-auto">
+                  {trainer.description}
+                </p>
+                <div className="flex gap-12 justify-center">
                   {trainer.social.map((social, index) => {
                     return (
                       <div key={index}>
                         <Link href={social.href}>
-                          <social.icon className="text-lg text-white" />
+                          <social.icon className="text-lg text-white hover:text-accent transition-all" />
                         </Link>
                       </div>
                     );
@@ -38,7 +60,19 @@ const Team = () => {
               </div>
             );
           })}
-        </div>
+        </motion.div>
+        <motion.div
+          className="py-12"
+          variants={fadeIn('up', 0.8)}
+          initial="childHidden"
+          whileInView={'childVisible'}
+          viewport={{ once: false, amount: 0.2 }}
+        >
+          <CustomButton
+            containerStyles="w-[196px] h-[62px] rounded-full"
+            text="See all trainers"
+          />
+        </motion.div>
       </div>
     </section>
   );
