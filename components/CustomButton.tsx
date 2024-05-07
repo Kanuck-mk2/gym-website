f7dfcdbac19927c791674d373929d148fed08be8
Kanuck-mk2 <141983009+Kanuck-mk2@users.noreply.github.com>
2024-05-07 01:12:18 -0600

    update before bed

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -10,7 +10,10 @@ const CustomButton = ({
       type="button"
       className={`${containerStyles} group relative cursor-pointer overflow-hidden bg-accent uppercase`}
     >
-      <span className="absolute top-1/2 -translate-x-20 ease bg-black duration-300 group-hover:h-64 group-hover:-translate-y-32 h-0 origin-center rotate-45 transition-all w-64"></span>
+      <span
+        className="absolute top-1/2 -translate-x-20  ease bg-black duration-300 
+      group-hover:h-64 group-hover:-translate-y-32 h-0 origin-center rotate-45 transition-all w-64"
+      ></span>
 
       <span className="ease relative text-black transition duration-300 group-hover:text-accent">
         {text}
